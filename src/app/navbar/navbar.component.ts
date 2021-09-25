231b9d6c1657daa010ba3a5756290f42e72de2a5
udyapatel <udya.patel2050@gmail.com>
2021-09-25 16:52:47 +0530

    2 files modified

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, NgZone, OnInit, Output } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   navbarOpen = false;
+  selected = true;
   constructor() { }
 
   ngOnInit() {
