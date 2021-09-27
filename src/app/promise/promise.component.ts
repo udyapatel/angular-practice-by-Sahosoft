026cc2af282b7830230d61fe21703b4a8681329f
udyapatel <udya.patel2050@gmail.com>
2021-09-27 14:37:50 +0530

    3 files modified

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promise.component.css']
 })
 export class PromiseComponent implements OnInit {
-
+  promiseVal;
   constructor() { }
 
+DellAvailable(){
+    return false;
+}
+HpAvailable(){
+    return false;
+}
+
+
   ngOnInit() {
+
+    let buyLaptop= new Promise((resolve,reject) =>{
+    //resolve('Promise is resolved')
+    //reject('Promise is reject')
+    if(this.DellAvailable()){
+      return setTimeout(() =>{
+        resolve('Dell is Purchased');
+      },3000)
+   
+    }
+    else if(this.HpAvailable()){
+      return setTimeout(() =>{
+        resolve('Hp is Purchased');
+      },3000)
+     
+    }
+    else{
+      return setTimeout(() =>{
+        reject('Laptop is not available in store');
+      },3000)
+    }
+    });
+    buyLaptop.then(res => {
+      console.log('then code executed => ' , res);
+      this.promiseVal =res ;
+    } ).catch(res =>{
+      console.log('catch code executed =>' , res);
+      this.promiseVal =res ;
+    })
+
   }
 
 }
