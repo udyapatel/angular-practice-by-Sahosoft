209d72ba856bc0c0563f21dcba704903fe128ceb
udyapatel <udya.patel2050@gmail.com>
2021-09-28 14:39:31 +0530

    4 files modified

--- src/app/form-event/form-event.component.ts
+++ src/app/form-event/form-event.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-form-event',
   templateUrl: './form-event.component.html',
   styleUrls: ['./form-event.component.css']
 })
-export class FormEventComponent implements OnInit {
-
+export class FormEventComponent implements OnInit, AfterViewInit{
+  @ViewChild('addBtn')  addBtn : ElementRef;
   constructor() { }
 
   ngOnInit() {
+
   }
+ngAfterViewInit(){
+
+  fromEvent(this.addBtn.nativeElement , 'click').subscribe(
+    res =>{
+      console.log(res);
+    }
+  )
+}
+
 
 }
