b146adc2b4206211209d04d57c258efdec5537bd
udyapatel <udya.patel2050@gmail.com>
2021-09-30 16:51:33 +0530

    2 files modified

--- src/app/of-from/of-from.component.ts
+++ src/app/of-from/of-from.component.ts
@@ -9,14 +9,23 @@ import { DesignUtilityService } from '../app-service/design-utility.service';
   styleUrls: ['./of-from.component.css']
 })
 export class OfFromComponent implements OnInit {
-
+obsMsg:any;
   constructor(private router:Router, private _designUtilityService:DesignUtilityService) { }
 
   ngOnInit() {
-    const obs1 = of('Udya' ,'Vaishnavi' , 'Avani Deo') ;
+    const obs1 = of('Udya Patel' ,'Vaishnavi Patel' , 'Avani Deo') ;
 
     obs1.subscribe(res =>{
       console.log(res);
+      this._designUtilityService.print(res ,'elContainer');
+    });
+
+    const obs2 = of({a:'Udya Patel' ,b: 'Vaishnavi Patel' , c:'Avani Deo'}) ;
+
+    obs2.subscribe(res =>{
+      this.obsMsg = res;
+      console.log('ObsMsg => ',res);
+     // this._designUtilityService.print(res ,'elContainer');
     });
   }
 
