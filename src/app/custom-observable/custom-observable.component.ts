8dc7ed8d839095494fc23f9d827d6168951e2a97
udyapatel <udya.patel2050@gmail.com>
2021-10-04 18:29:44 +0530

    4 files modified

--- src/app/custom-observable/custom-observable.component.ts
+++ src/app/custom-observable/custom-observable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DesignUtilityService } from '../app-service/design-utility.service';
 
 @Component({
   selector: 'app-custom-observable',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-observable.component.css']
 })
 export class CustomObservableComponent implements OnInit {
-
-  constructor() { }
+techStatus;
+  constructor(private _designUtilityService:DesignUtilityService) { }
 
   ngOnInit() {
+
+    //Example 1 (Manual)
+    const cusObs1 = Observable.create(observer => {
+      setTimeout(()=>{
+        observer.next('Angular')
+      },1000);
+      setTimeout(()=>{
+        observer.next('CSS3')
+      },2000);
+      setTimeout(()=>{
+        observer.next('Bootstrap')
+      },3000);
+      setTimeout(()=>{
+        observer.next('Java')
+      //  observer.error(new Error('Limit Execeeded'));
+      },4000);
+      setTimeout(()=>{
+        observer.next('Jquery')
+        observer.complete()
+      },4000);
+
+    });
+    cusObs1.subscribe(res=>{
+      console.log(res);
+      this._designUtilityService.print(res,'elContainer');
+     (err =>{
+      this.techStatus = 'error';
+     });
+     (()=>{
+      this.techStatus = 'completed';
+     })
+
+    });
   }
 
 }
