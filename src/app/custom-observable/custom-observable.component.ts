7345c91bb4116ecf92c80a678d3291306d746f9f
udyapatel <udya.patel2050@gmail.com>
2021-10-05 16:10:02 +0530

    1 file modified

--- src/app/custom-observable/custom-observable.component.ts
+++ src/app/custom-observable/custom-observable.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { count } from 'rxjs/operators';
 import { DesignUtilityService } from '../app-service/design-utility.service';
 
 @Component({
@@ -7,8 +8,9 @@ import { DesignUtilityService } from '../app-service/design-utility.service';
   templateUrl: './custom-observable.component.html',
   styleUrls: ['./custom-observable.component.css']
 })
-export class CustomObservableComponent implements OnInit {
+export class CustomObservableComponent implements OnInit, {
 techStatus;
+sub2:Subscription;
   constructor(private _designUtilityService:DesignUtilityService) { }
 
   ngOnInit() {
@@ -45,6 +47,31 @@ techStatus;
      })
 
     });
-  }
+
+    //Example 2 (Custom Observables)
+
+
+    // const cusObs2 = Observable.create(observer =>{
+    //   let count = 1
+    //  setInterval(()=>{
+    //  observer.next('Data emit' +count);
+    //  count++;
+
+    //  if(count >= 5){
+    //    observer.complete();
+    //  }
+    //  },10000)
+    // })
+   //this.sub2  = cusObs2.subscribe(res =>{
+     // console.log(res);
+     // this._designUtilityService.print(res, 'elContainer');
+   //})
+
+   }
+   //Example 3 (Random Names)
+  //  ngOnDestroy(){
+  //    this.sub2.unsubscribe();
+
+  //  }
 
 }
